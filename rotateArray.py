eded2f88bcbab01dd7aa48984be40f9d57e3cabb
Anthony Ho <anthony.ho@shopify.com>
2019-06-12 22:35:15 -0400

    2019/06/12

---
+++ rotateArray.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        def reverse(nums, start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        n = len(nums)
+        k = k % n
+        if k == 0:
+            return
+        reverse(nums, 0, n-1)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, n-1)
